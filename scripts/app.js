f269f15dc43e5aee1657a519a38612c8e59649ec
David <davidlopezzurron@gmail.com>
2026-03-15 03:47:28 +0100

    iconos sol y luna puestos, funcionalidad cambio terminada

--- scripts/app.js
+++ scripts/app.js
@@ -1,15 +1,21 @@
 //Cambiar al darkMode
 const radioDia = document.getElementById('modoDia');
 const radioNoche = document.getElementById('modoNoche');
+const iconoSol = document.getElementById('iconoSol');
+const iconoLuna = document.getElementById('iconoLuna');
 
 radioDia.addEventListener('change', () => {
     if (radioDia.checked) {
         document.body.classList.remove('darkMode');
+        iconoSol.src = "assets/img/sol-dia.png";
+        iconoLuna.src = "assets/img/luna-dia.png";
     }
 });
 
 radioNoche.addEventListener('change', () => {
     if (radioNoche.checked) {
         document.body.classList.add('darkMode');
+        iconoSol.src = "assets/img/sol-noche.png";
+        iconoLuna.src = "assets/img/luna-noche.png";
     }
 });
