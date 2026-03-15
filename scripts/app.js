e3ae323e59927fd5f55928672dbe4007bfe800a4
rodrii-garciia <rodrii.garciia.yt@gmail.com>
2026-03-15 11:39:03 +0100

    creación de tarjetas de peleadores

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,4 @@
-//Cambiar al darkMode
+// Cambiar al darkMode
 const radioDia = document.getElementById('modoDia');
 const radioNoche = document.getElementById('modoNoche');
 const iconoSol = document.getElementById('iconoSol');
@@ -19,3 +19,54 @@ radioNoche.addEventListener('change', () => {
         iconoLuna.src = "assets/img/luna-noche.png";
     }
 });
+
+// Extraer peleadores del XML
+const peleadores = [];
+fetch("data/peleadores.xml")
+    .then(peleador => peleador.text())
+    .then(xmlString => {
+
+        const parser = new DOMParser();
+        const xml = parser.parseFromString(xmlString, "text/xml");
+
+        const peleadoresXML = xml.getElementsByTagName("peleador");
+        const lista = document.querySelector(".listaPeleadores");
+
+        for(let i = 0; i < peleadoresXML.length; i++){
+
+            const id = "peleador" + (i+1)
+            const nombre = peleadoresXML[i].getElementsByTagName("nombre")[0].textContent;
+            const apellido = peleadoresXML[i].getElementsByTagName("apellido")[0].textContent;
+            const alias = peleadoresXML[i].getElementsByTagName("alias")[0].textContent;
+            const pais = peleadoresXML[i].getElementsByTagName("pais")[0].textContent;
+            const cita = peleadoresXML[i].getElementsByTagName("cita")[0].textContent;
+            const imagenUrl = peleadoresXML[i].getElementsByTagName("imagenUrl")[0].textContent;
+
+            const elemento = document.createElement("li");
+
+            elemento.innerHTML = `
+                <img src="${imagenUrl}" alt="Imagen de ${nombre} ${apellido}">
+                <h2>${nombre} ${apellido}</h2>
+                <p class="alias">${alias}</p>
+                <p class="pais">${pais}</p>
+                <p class="cita">${cita}</p>
+            `
+
+            elemento.classList.add("tarjeta");
+
+            lista.appendChild(elemento);
+
+            // este array se crea para usarse en el filtrado
+            peleadores.push(
+                {
+                    id: id,
+                    nombre: nombre,
+                    apellido: apellido,
+                    alias: alias,
+                    pais: pais,
+                    cita: cita,
+                    imagenUrl: imagenUrl
+                }
+            )
+        }
+    })
